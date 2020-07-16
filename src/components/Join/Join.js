d2175de10c7a1fd1a0c63982d0022a4f51c0d615
SupriyaShekar123 <s.supriya82@gmail.com>
2020-07-16 14:54:17 +0200

    installed material UI

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEfffect, useState } from "react";
 
 export default function Join() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
   return <div>Join</div>;
 }
