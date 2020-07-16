96afac6ec84028311c881eb3a2dd8f2c40657698
SupriyaShekar123 <s.supriya82@gmail.com>
2020-07-16 14:47:15 +0200

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
-import Voice from "./components/Voice";
+
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
 
 function App() {
   return (
-    <div className='App'>
-      <Voice />;
-    </div>
+    <Router>
+      <Route path='/' exact component={Join} />
+      <Route path='/chat' component={Chat} />
+    </Router>
   );
 }
 
